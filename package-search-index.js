36edcbf473102ad60a616437044cc1d3872f4f41
Jotrorox <jotrorox@gmail.com>
2024-05-17 16:03:11 +0000

    Deploying to gh-pages from @ Jotrorox/JToml@4d40fff96ae308bfed0a0ed45120175b24e6228b 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jotrorox.jtoml"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jotrorox.jtoml"},{"l":"com.jotrorox.jtoml.types"}];updateSearchResults();
